2f771b437bdbc29b417cfaa41b67b27557c86072
Your-Pluviophile <935664910@qq.com>
2022-11-13 11:50:53 +0800

    使用ik进行词频统计

---
+++ src/main/java/com/fudan/wordcount/TokenizerMapper.java
@@ -0,0 +1,32 @@
+package com.fudan.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+
+import java.io.*;
+
+public class TokenizerMapper extends
+        Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        byte[] bt = value.getBytes();
+        InputStream ip = new ByteArrayInputStream(bt);
+        Reader read = new InputStreamReader(ip);
+        IKSegmenter iks = new IKSegmenter(read, true);
+        Lexeme t;
+        while ((t = iks.next()) != null) {
+            word.set(t.getLexemeText());
+            context.write(word, one);
+        }
+    }
+}
+
+
